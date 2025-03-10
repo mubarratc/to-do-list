fe4722be25d101fe07a8548302789256e1147b77
mubarratc <mubarratc@mercy.ngo>
2025-03-09 23:22:24 -0500

    start

---
+++ index.js
@@ -0,0 +1,52 @@
+// import "styles.css";
+
+// Decalring HTML Elements as variables
+const buttonEL = document.getElementById("btn-el");
+const input = document.getElementById("input-el");
+const list = document.getElementById("list-el");
+const li = document.getElementById("li-el");
+const deleteButton = document.querySelectorAll("delete-el");
+let tasks = [];
+
+//adding an event listener that calls a function that reads the value from the textt box input and then add it's to a unordered list.
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    console.log("The Enter Key was pressed!");
+  } else {
+    console.log("Didn't recognize enter!");
+  }
+});
+
+buttonEL.addEventListener("click", function () {
+  let inputValue = input.value;
+  addTask(inputValue); //adds tasks to the front-end
+  tasks.push(inputValue); //adds tasks to an array
+  console.log(tasks);
+  clearInput();
+});
+
+list.addEventListener("click", function (element) {
+  if (element.target.classList.contains("delete-el")) {
+    const listItem = element.target.closest("li");
+    listItem.remove();
+  }
+});
+
+function addTask(input) {
+  let html = `<li class="list-el"><span> ${input}</span> <button class="delete-el">Delete</button></li>`;
+  if (input === "") {
+    return "";
+  } else {
+    list.innerHTML += html;
+  }
+}
+
+function clearInput() {
+  input.value = "";
+}
+
+function deleteTask() {
+  let li = this.parentNode;
+  list.removeChild(li);
+}
